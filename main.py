ddf6a7a7059a7d2f7f98f852305cfecea2a32be9
Ming-der Wang <mingder78@yahoo.com>
2020-11-21 17:10:33 +0000

    add class method getInstance()

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from singleton.america_president import AmericaPresident
 class App:
     def main():
         a = AmericaPresident()
-        b = AmericaPresident()
+        b = AmericaPresident.getInstance()
+        c = a.getInstance()
         print(id(a))
         print(id(b))
+        print(id(c))
 
 
 if __name__ == '__main__':
--- singleton/america_president.py
+++ singleton/america_president.py
@@ -1,5 +1,8 @@
 from pypattyrn.creational.singleton import Singleton
 
 class AmericaPresident(object, metaclass=Singleton):
+    @classmethod
+    def getInstance(self) -> Singleton:
+        return AmericaPresident()
     def say_something(self) -> None:
         return "I'm the only president of the United States."
