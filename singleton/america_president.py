0aa14149912a8361a3c5a1fead54aaf4d2a85a21
Ming-der Wang <mingder78@yahoo.com>
2020-11-21 17:25:12 +0000

    change Singleton

--- singleton/america_president.py
+++ singleton/america_president.py
@@ -1,8 +1,16 @@
-from pypattyrn.creational.singleton import Singleton
+class Singleton(object):
+    _instance = None
 
-class AmericaPresident(object, metaclass=Singleton):
+    def __new__(class_, *args, **kwargs):
+        if not isinstance(class_._instance, class_):
+            class_._instance = object.__new__(class_, *args, **kwargs)
+        return class_._instance
+
+
+class AmericaPresident(Singleton):
     @classmethod
-    def getInstance(self) -> Singleton:
+    def getInstance(self):
         return AmericaPresident()
+
     def say_something(self) -> None:
         return "I'm the only president of the United States."
